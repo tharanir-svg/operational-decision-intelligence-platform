c82ad71b15e9516b09e02e519bb88679bf49e2de
tharanir-svg <tharani.r@cognizant.com>
2026-06-24 19:04:31 +0530

    Create ThresholdEngine.js

---
+++ src/engine/ThresholdEngine.js
@@ -0,0 +1,43 @@
+export default class ThresholdEngine {
+  constructor(thresholdMatrix) {
+    this.thresholdMatrix = thresholdMatrix;
+  }
+
+  evaluate(eventContext) {
+    const matchingRule = this.thresholdMatrix.rules.find(rule => {
+      if (rule.eventType !== eventContext.eventType) {
+        return false;
+      }
+
+      if (
+        rule.conditions?.fatalities?.gte &&
+        eventContext.fatalities <
+          rule.conditions.fatalities.gte
+      ) {
+        return false;
+      }
+
+      if (
+        rule.conditions?.injuries?.gte &&
+        eventContext.injuries <
+          rule.conditions.injuries.gte
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    if (!matchingRule) {
+      return {
+        severity: 1,
+        action: "MONITOR"
+      };
+    }
+
+    return {
+      severity: matchingRule.recommendedSeverity,
+      action: matchingRule.recommendedAction
+    };
+  }
+}
