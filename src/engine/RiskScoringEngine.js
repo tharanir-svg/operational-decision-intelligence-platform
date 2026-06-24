269ca6b0be3e00b66d08eabf8d025e682a4f6344
tharanir-svg <tharani.r@cognizant.com>
2026-06-24 19:04:50 +0530

    Create RiskScoringEngine.js

---
+++ src/engine/RiskScoringEngine.js
@@ -0,0 +1,29 @@
+export default class RiskScoringEngine {
+  constructor(weightLibrary, regionProfiles) {
+    this.weights = weightLibrary.weights;
+    this.regionProfiles = regionProfiles.regions;
+  }
+
+  calculate(eventContext) {
+    let score = 0;
+
+    score += (eventContext.fatalities || 0) *
+      (this.weights.Fatalities || 0);
+
+    score += (eventContext.injuries || 0) *
+      (this.weights.Injuries || 0);
+
+    const region = this.regionProfiles.find(
+      r => r.region === eventContext.region
+    );
+
+    if (region) {
+      score += region.baselineRisk * 10;
+    }
+
+    return {
+      riskScore: Math.min(score, 100),
+      confidence: 90
+    };
+  }
+}
