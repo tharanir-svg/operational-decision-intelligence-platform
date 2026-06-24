87ae59c5ea158633b152a9c8ce3109be6772b76e
tharanir-svg <tharani.r@cognizant.com>
2026-06-24 19:04:07 +0530

    Create PolicyEngine.js

---
+++ src/policy/PolicyEngine.js
@@ -0,0 +1,16 @@
+export default class PolicyEngine {
+  constructor(policyLibrary) {
+    this.policyLibrary = policyLibrary;
+  }
+
+  evaluate(eventContext) {
+    return this.policyLibrary.policies.filter(policy => {
+      if (!policy.appliesTo) return true;
+
+      return (
+        policy.appliesTo.includes("All") ||
+        policy.appliesTo.includes(eventContext.domain)
+      );
+    });
+  }
+}
