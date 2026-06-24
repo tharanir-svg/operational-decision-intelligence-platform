d1971064657600da506579a95a3a44e2fa9aa04a
tharanir-svg <tharani.r@cognizant.com>
2026-06-24 18:46:25 +0530

    Create KnowledgeLoader.js
    Load all knowledge assets from repository

---
+++ src/knowledge/KnowledgeLoader.js
@@ -0,0 +1,14 @@
+export class KnowledgeLoader {
+    constructor() {
+        this.entities = [];
+        this.events = [];
+        this.modifiers = [];
+        this.policies = [];
+        this.relationships = [];
+        this.regions = [];
+    }
+
+    async load() {
+        // load all json assets
+    }
+}
